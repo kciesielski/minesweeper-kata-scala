6efabf70a4dd21791e34d44b42536a6ce18380e5
Krzysztof Ciesielski <panciesielski@gmail.com>
2012-12-03 20:01:15 +0100

    Solution builder

--- src/main/scala/org/minesweeperkata/GameDef.scala
+++ src/main/scala/org/minesweeperkata/GameDef.scala
@@ -1,24 +1,55 @@
 package org.minesweeperkata
 
+import org.minesweeperkata.Pos._
+
+object Pos {
+  type Move = Pos => Pos
+  val moveUp: Move = (pos: Pos) => pos.dy(-1)
+  val moveRight: Move = (pos: Pos) => pos.dx(1)
+  val moveLeft: Move = (pos: Pos) => pos.dx(-1)
+  val moveDown: Move = (pos: Pos) => pos.dy(1)
+
+  lazy val moveRightUp = (p: Pos) => moveRight(moveUp(p))
+  lazy val moveRightDown = (p: Pos) => moveRight(moveDown(p))
+  lazy val moveLeftUp = (p: Pos) => moveLeft(moveUp(p))
+  lazy val moveLeftDown = (p: Pos) => moveLeft(moveDown(p))
+
+}
+
 case class Pos(x: Int, y: Int) {
 
   def dx(d: Int) = copy(x = x + d, y)
   def dy(d: Int) = copy(x, y = y + d)
 }
 
+object GameDef {
+
+  def solution(level: GameDef): IndexedSeq[IndexedSeq[Int]] = {
+
+    for (row <- 0 to level.height - 1) yield
+      for ( col <- 0 to level.width - 1) yield
+        level.hint((Pos(col, row)))
+  }
+}
+
 trait GameDef {
 
   type Terrain = Pos => Boolean
   type MineLayout = Pos => Boolean
-
+  val moves: List[Move] = List(moveRight, moveUp, moveLeft, moveDown, moveRightDown, moveRightUp, moveLeftUp, moveLeftDown)
   val width: Int
   val height: Int
+  val mine: MineLayout
 
   def terrain(position: Pos) : Boolean = {
     position.x >= 0 && position.x < width && position.y >= 0 && position.y < height
   }
 
-  val mine: MineLayout
-
-
+  def hint(pos: Pos): Int = {
+    moves.foldLeft(0)((count, move) => {
+      val isMine = if (mine(move(pos))) 1 else 0
+      count + isMine
+    })
+  }
 }
+
--- src/test/scala/org/minesweepkata/GameTest.scala
+++ src/test/scala/org/minesweepkata/GameTest.scala
@@ -4,20 +4,33 @@ import org.specs2.mutable._
 import org.specs2.matcher.ShouldMatchers
 
 import org.minesweeperkata.{Pos, GameDef, StringParserTerrain}
+import org.minesweeperkata.Pos._
 
 class GameTest extends Specification with ShouldMatchers {
+
+  object TrivialEmptyLevel extends GameDef with StringParserTerrain {
+    override val level =
+      """.""".stripMargin
+
+    val solution =
+      """0""".stripMargin
+
+    override val width = 1
+    override val height = 1
+  }
+
 
   object Level1 extends GameDef with StringParserTerrain {
     override val level =
       """...
         |.*.
         |...""".stripMargin
+
     override val width = 3
     override val height = 3
   }
 
   object Level2 extends GameDef with StringParserTerrain {
-
 
     override val level =
       """.*.
@@ -27,39 +40,102 @@ class GameTest extends Specification with ShouldMatchers {
     override val height = 3
   }
 
+  object Level3 extends GameDef with StringParserTerrain {
+
+    override val level =
+      """.*.*
+        |..**
+        |..*.""".stripMargin
+
+    val solution =
+    Vector(
+    Vector(1,1,4,2),
+    Vector(1,3,4,3),
+    Vector(0,2,2,3))
+
+    override val width = 4
+    override val height = 3
+  }
+
 
   "terrain function for level 1" should {
     "correctly determine if position belongs to terrain" in {
 
       val level = Level1
-      assert(level.terrain(Pos(1,1)))
-      assert(level.terrain(Pos(2,2)))
-      assert(level.terrain(Pos(2,0)))
-      assert(level.terrain(Pos(0,0)))
-      assert(level.terrain(Pos(1,2)))
+      assert(level.terrain(Pos(1, 1)))
+      assert(level.terrain(Pos(2, 2)))
+      assert(level.terrain(Pos(2, 0)))
+      assert(level.terrain(Pos(0, 0)))
+      assert(level.terrain(Pos(1, 2)))
 
-      assert(!level.terrain(Pos(1,3)))
-      assert(!level.terrain(Pos(-1,1)))
+      assert(!level.terrain(Pos(1, 3)))
+      assert(!level.terrain(Pos(-1, 1)))
     }
   }
 
-    "mine function for level 1" should {
-      "correctly determine if position has a mine" in {
+  "mine function for level 1" should {
+    "correctly determine if position has a mine" in {
 
-        val level = Level1
-        assert(level.mine(Pos(1,1)))
-        assert(!level.mine(Pos(0,1)))
-        assert(!level.mine(Pos(-1,1)))
-      }
+      val level = Level1
+      assert(level.mine(Pos(1, 1)))
+      assert(!level.mine(Pos(0, 1)))
+      assert(!level.mine(Pos(-1, 1)))
     }
+  }
 
   "mine function for level 2" should {
     "correctly determine if position has a mine" in {
 
       val level = Level2
-      assert(level.mine(Pos(1,0)))
-      assert(!level.mine(Pos(1,1)))
-      assert(!level.mine(Pos(-1,1)))
+      assert(level.mine(Pos(1, 0)))
+      assert(!level.mine(Pos(1, 1)))
+      assert(!level.mine(Pos(-1, 1)))
+    }
+  }
+
+  "hint for single field" should {
+    "be always 0" in {
+      val level = TrivialEmptyLevel
+      level.hint(Pos(0, 0)) === 0
+    }
+  }
+
+  "hint for field" should {
+    "be number of surrounding mines" in {
+      val level1 = Level1
+      val level2 = Level2
+      val level3 = Level3
+
+      level1.hint(Pos(1, 0)) === 1
+      level1.hint(Pos(0, 0)) === 1
+
+      level2.hint(Pos(0, 2)) === 0
+
+      level3.hint(Pos(0, 2)) === 0
+      level3.hint(Pos(2, 0)) === 4
+      level3.hint(Pos(1, 0)) === 1
+      level3.hint(Pos(1, 1)) === 3
+      level3.hint(Pos(0, -1)) === 1
+    }
+  }
+
+  "moving functions" should {
+    "be composable" in {
+      val pos = Pos(1, 1)
+      lazy val rightUp = (p: Pos) => moveRight(moveUp(p))
+      rightUp(pos) === Pos(2, 0)
+    }
+  }
+
+  "solution for trivial level 0" should {
+    "0" in {
+      GameDef.solution(TrivialEmptyLevel) === Vector(Vector(0))
+    }
+  }
+
+  "solution for level 3" should {
+    "be as expected" in {
+      GameDef.solution(Level3) === Level3.solution
     }
   }
 
