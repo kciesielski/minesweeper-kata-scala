095087bf7bc99fee797804a46ba098c5a8aa339a
Krzysztof Ciesielski <panciesielski@gmail.com>
2012-12-19 23:24:24 +0100

    Server facade scaffolding

---
+++ src/main/scala/org/minesweeperkata/webfacade/MinesweeperFacade.scala
@@ -0,0 +1,27 @@
+package org.minesweeperkata.webfacade
+
+import org.minesweeperkata.Pos
+
+class MinesweeperFacade {
+
+  // http post to put a step step on a tile
+  def stepOn(pos: Pos) {
+
+  }
+
+  // get for tile value if previously stepped on it
+  def tile(pos: Pos): String = {
+    "*"
+  }
+
+  // get for game status
+  def status(): String = {
+    "TODO"
+  }
+
+  // get for random level
+  def randomLevel() {
+
+  }
+
+}
