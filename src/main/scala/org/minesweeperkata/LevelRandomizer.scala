530858067ae22a4707903740e0502271aeece539
Krzysztof Ciesielski <panciesielski@gmail.com>
2012-12-21 22:22:47 +0100

    Complete level randomizer

--- src/main/scala/org/minesweeperkata/LevelRandomizer.scala
+++ src/main/scala/org/minesweeperkata/LevelRandomizer.scala
@@ -1,22 +1,43 @@
 package org.minesweeperkata
 
 import util.Random
+import annotation.tailrec
 
-class LevelRandomizer(val randomInts: Stream[Int] = Stream continually(Random.nextInt())) {
+class LevelRandomizer(val randomInts: Stream[Int] = Stream continually (Random.nextInt())) {
 
 
   def getNextLevel(newWidth: Int, newHeight: Int, mineCount: Int): GameDef = {
     new GameDef {
-      val height: Int = height
-      val width: Int = width
-      val minePositions = randomInts.take(mineCount)
-      val mine: this.type#MineLayout = {
-
-
-        pos => {
-          (pos == Pos(1, 1) || pos == Pos(2, 2) || pos == Pos(3, 3) || pos == Pos(4, 4) || pos == Pos(5, 5))
-        }
-      }
+      val height: Int = newHeight
+      val width: Int = newWidth
+      val emptyLayout = (pos: Pos) => false
+      override val mine = randomPosition(randomInts, mineCount, newWidth, newHeight, emptyLayout)
     }
+  }
+
+  @tailrec
+  private def randomPosition(stream: Stream[Int], mineCount: Int, width: Int, height: Int, layout: GameDef#MineLayout): GameDef#MineLayout = {
+
+    val newData = nextNotRepeated(stream, width, height, layout)
+    val rest = newData._2
+    val newPos = newData._1
+    val newLayout = (pos: Pos) => layout(pos) || (pos == newPos)
+    if (mineCount == 1)
+      newLayout
+    else
+      randomPosition(rest, mineCount - 1, width, height, newLayout)
+  }
+
+  @tailrec
+  private def nextNotRepeated(stream: Stream[Int], width: Int, height: Int, currentLayout: GameDef#MineLayout): (Pos, Stream[Int]) = {
+    val first = stream.head % width
+    val second = stream.tail.head % height
+    val rest = stream.tail
+    val newPos = Pos(first, second)
+    if (currentLayout(newPos)) {
+      nextNotRepeated(rest, width, height, currentLayout)
+    }
+    else
+      (newPos, rest.tail)
   }
 }
--- src/test/scala/org/minesweeperkata/LevelRandomizerTest.scala
+++ src/test/scala/org/minesweeperkata/LevelRandomizerTest.scala
@@ -6,25 +6,53 @@ class LevelRandomizerTest extends Specification {
 
 
   "Level Randomizer" should {
+
     "build a simple level" in {
 
-      val randomNumbers = Stream(1, 1, 2, 2, 3, 3, 4, 4, 5, 5)
+      val randomNumbers = Stream(0, 0, 1, 1, 2, 2, 3, 3, 4, 4)
       val expectedMineLayout: Pos => Boolean = {
         pos => {
-          (pos == Pos(1, 1) || pos == Pos(2, 2) || pos == Pos(3, 3) || pos == Pos(4, 4) || pos == Pos(5, 5))
+          (pos == Pos(0, 0) || pos == Pos(1, 1) || pos == Pos(2, 2) || pos == Pos(3, 3) || pos == Pos(4, 4))
         }
       }
-        val randomizer = new LevelRandomizer(randomNumbers)
+      val randomizer = new LevelRandomizer(randomNumbers)
 
-        val width = 5
-        val height = 6
-        val nextLevel = randomizer.getNextLevel(width, height, 5)
-        val generatedMineLayout = nextLevel.mine
+      val width = 5
+      val height = 6
+      //when
+      val nextLevel = randomizer.getNextLevel(width, height, 5)
+      // then
+      val generatedMineLayout = nextLevel.mine
 
-        for (x <- 0 to width;
-             y <- 0 to height) {
-          generatedMineLayout(Pos(x, y)) must beEqualTo(expectedMineLayout(Pos(x, y)))
-        }
+      for (x <- 0 to width - 1;
+           y <- 0 to height - 1) {
+        generatedMineLayout(Pos(x, y)) must beEqualTo(expectedMineLayout(Pos(x, y)))
+      }
     }
+
+    "not allow repeated mine positions" in {
+
+      val randomNumbers = Stream(0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 3)
+      val expectedMineLayout: Pos => Boolean = {
+        pos => {
+          (pos == Pos(0, 0) || pos == Pos(1, 1) || pos == Pos(1, 2) || pos == Pos(2, 3) || pos == Pos(3, 3))
+        }
+      }
+      val randomizer = new LevelRandomizer(randomNumbers)
+
+      val width = 5
+      val height = 6
+      // when
+      val nextLevel = randomizer.getNextLevel(width, height, 5)
+
+      // then
+      val generatedMineLayout = nextLevel.mine
+
+      for (x <- 0 to width - 1;
+           y <- 0 to height - 1) {
+        generatedMineLayout(Pos(x, y)) must beEqualTo(expectedMineLayout(Pos(x, y)))
+      }
+    }
+
   }
 }
