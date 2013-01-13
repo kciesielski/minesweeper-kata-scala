a909aa16dd2624d6088368ebdadec6a0cc0e1178
Krzysztof Ciesielski <panciesielski@gmail.com>
2013-01-13 22:09:58 +0100

    Add REST facade for generating new level

--- src/main/scala/org/minesweeperkata/rest/MinesweeperServlet.scala
+++ src/main/scala/org/minesweeperkata/rest/MinesweeperServlet.scala
@@ -5,6 +5,7 @@ import common.SafeInt
 import org.scalatra.databinding.JacksonJsonParsing
 import org.scalatra.json.{JValueResult, JacksonJsonSupport}
 import org.scalatra.ScalatraServlet
+import rest.NewGame
 
 class MinesweeperServlet(val levelRandomizer: LevelRandomizer = new LevelRandomizer(), val gameStateDao: GameStateDao = InMemoryGameStateDao)
   extends ScalatraServlet with JacksonJsonParsing with JacksonJsonSupport with JValueResult {
@@ -33,7 +34,8 @@ class MinesweeperServlet(val levelRandomizer: LevelRandomizer = new LevelRandomi
   }
 
   post("/randomLevel") {
-
+    val params = parsedBody.extract[NewGame]
+    randomize(params.width, params.height, params.mineCount, levelRandomizer, gameStateDao)
   }
 
   get("/status") {
---
+++ src/main/scala/org/minesweeperkata/rest/NewGame.scala
@@ -0,0 +1,3 @@
+package org.minesweeperkata.rest
+
+case class NewGame(width: Int, height: Int, mineCount: Int)
