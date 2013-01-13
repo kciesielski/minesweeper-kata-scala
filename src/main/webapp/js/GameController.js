c2ee32d2b52630b8f6d163df57679b104ba99ee5
Krzysztof Ciesielski <panciesielski@gmail.com>
2013-01-13 22:10:42 +0100

    Add simple generation of new game board on view side

--- src/main/webapp/js/GameController.js
+++ src/main/webapp/js/GameController.js
@@ -1,11 +1,37 @@
 function GameController($scope, $location) {
 
-    $scope.newGame = function(width, height, mines) {
-
+    $scope.startNew = function () {
+        console.log("TODO server call")
+        $scope.resetBoard()
     }
 
-    $scope.board = {
+    $scope.resetBoard = function () {
+        $scope.board = {
+            "rows": {}
+        }
+
+        var width = $scope.newGame.width
+        var height = $scope.newGame.height
+
+        for (var i=0; i<height; i++)
+        {
+            $scope.board.rows[i] = []
+            var row = $scope.board.rows[i]
+            row.cols = []
+            for (var c=0; c<width; c++)
+            {
+                row.cols[c] = {
+                    value: "?"
+                }
+            }
+        }
     }
 
+    $scope.newGame = {
+        "width": 8,
+        "height": 8,
+        "mineCount": 8
+    }
 
+    $scope.resetBoard()
 }
